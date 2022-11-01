1853112154898c8f9b71d46c1a5221e2371e1a05
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2022-11-01 17:58:03 +0100

    Allow to add new authors

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,4 +13,12 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should allow to submit new authors" do
+    visit new_author_path
+    fill_in 'author[first_name]', with: 'Alan'
+    fill_in 'author[last_name]', with: 'Turing'
+    fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+  end
 end
