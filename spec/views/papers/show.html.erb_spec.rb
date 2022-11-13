1b61600f5e3bd4d1a27f8d186f8e3264ecb1cdea
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2022-11-13 14:44:37 +0100

    Show authors in paper page

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "papers/show", type: :view do
   before(:each) do
-    assign(:paper, Paper.create!(
+    @paper = Paper.create!(
       title: "Title",
       venue: "Venue",
-      year: 2
-    ))
+      year: 2,
+      authors: [Author.create!(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")]
+    )
+    assign(:paper, @paper)
   end
 
   it "renders attributes in <p>" do
@@ -14,5 +16,10 @@ RSpec.describe "papers/show", type: :view do
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Venue/)
     expect(rendered).to match(/2/)
+  end
+
+  it "should have a link to the author's page" do
+    render
+    expect(rendered).to have_link("Alan Turing", href: author_path(@paper.authors.first))
   end
 end
