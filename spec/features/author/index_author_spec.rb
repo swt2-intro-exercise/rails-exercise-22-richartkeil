7c9967123440235c52e050fb20f8fa38c9c77ded
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2022-11-13 12:32:29 +0100

    Allow to delete authors

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -10,12 +10,27 @@ describe "Index author page", type: :feature do
     expect(page).to have_text(author_one.homepage)
     expect(page).to have_link('Show', href: author_path(author_one))
     expect(page).to have_link('Edit', href: edit_author_path(author_one))
+    expect(page).to have_link('Delete', href: author_path(author_one))
 
     expect(page).to have_text(author_two.name)
     expect(page).to have_text(author_two.homepage)
     expect(page).to have_link('Show', href: author_path(author_two))
     expect(page).to have_link('Edit', href: edit_author_path(author_two))
+    expect(page).to have_link('Delete', href: author_path(author_two))
 
     expect(page).to have_link 'New Author', href: new_author_path
+  end
+
+  it "should allow to delete authors" do
+    author = FactoryBot.create :author
+    visit authors_path()
+
+    expect(Author.count).to eq(1)
+    expect(page).to have_text(author.name)
+
+    find('a[data-turbo-method="delete"][href="' + author_path(author) + '"]').click
+
+    expect(Author.count).to eq(0)
+    expect(page).not_to have_text(author.name)
   end
 end
