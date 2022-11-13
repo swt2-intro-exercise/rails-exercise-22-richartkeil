113e299c80b52246f7fd2fc2f6d5c0b810cee088
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2022-11-13 12:22:41 +0100

    Update links

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -8,8 +8,13 @@ describe "Index author page", type: :feature do
 
     expect(page).to have_text(author_one.name)
     expect(page).to have_text(author_one.homepage)
+    expect(page).to have_link('Show', href: author_path(author_one))
+    expect(page).to have_link('Edit', href: edit_author_path(author_one))
+
     expect(page).to have_text(author_two.name)
     expect(page).to have_text(author_two.homepage)
+    expect(page).to have_link('Show', href: author_path(author_two))
+    expect(page).to have_link('Edit', href: edit_author_path(author_two))
 
     expect(page).to have_link 'New Author', href: new_author_path
   end
